5376fa55b09f72e8f5ec527eedf3513d5f952504
Aken Mehta <mehta.aken@gmail.com>
2017-08-15 16:24:22 -0400

    added main section => main-image-carousel + partners + case-study

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,56 +1,74 @@
 var app = {};
 
+app.partners = function() {
+	$('.partners').slick({
+		  infinite: true,
+		  slidesToShow: 4,
+		  slidesToScroll: 4
+		});
+};
+app.mainImageCarousel = function() {
+	// $('.main-image-carousel').slick({
+	//   dots: true,		
+	//   centerMode: true,
+	//   centerPadding: '60px',
+	//   slidesToShow: 5,
+
+	//   responsive: [
+	//   	{
+	//       breakpoint: 1500,
+	//       settings: {
+	//         arrows: false,
+	//         centerMode: true,
+	//         centerPadding: '40px',
+	//         slidesToShow: 4
+	//       }
+	//     },
+	//     {
+	//       breakpoint: 1200,
+	//       settings: {
+	//         arrows: false,
+	//         centerMode: true,
+	//         centerPadding: '40px',
+	//         slidesToShow: 3
+	//       }
+	//     },
+	//     {
+	//       breakpoint: 860,
+	//       settings: {
+	//         arrows: false,
+	//         centerMode: true,
+	//         centerPadding: '40px',
+	//         slidesToShow: 2
+	//       }
+	//     },
+	//     {
+	//       breakpoint: 480,
+	//       settings: {
+	//         arrows: false,
+	//         centerMode: true,
+	//         centerPadding: '40px',
+	//         slidesToShow: 1
+	//       }
+	//     }
+	//   ]
+	// });
+	$('.main-image-carousel').slick({
+	 	dots: true,
+	 	infinite: true,
+	 	speed: 300,
+	 	slidesToShow: 1,
+	 	centerMode: true,
+	 	variableWidth: true,
+	 	arrows: false,
+	 	focusOnSelect: true,
+	 	draggable: true,
+	});
+};
+
 app.init = function() {
-	console.log('connected app.init');
-
-
-
-$('.main-image-carousel').slick({
-  dots: true,		
-  centerMode: true,
-  centerPadding: '60px',
-  slidesToShow: 5,
-
-  responsive: [
-  	{
-      breakpoint: 1500,
-      settings: {
-        arrows: false,
-        centerMode: true,
-        centerPadding: '40px',
-        slidesToShow: 4
-      }
-    },
-    {
-      breakpoint: 1200,
-      settings: {
-        arrows: false,
-        centerMode: true,
-        centerPadding: '40px',
-        slidesToShow: 3
-      }
-    },
-    {
-      breakpoint: 860,
-      settings: {
-        arrows: false,
-        centerMode: true,
-        centerPadding: '40px',
-        slidesToShow: 2
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        arrows: false,
-        centerMode: true,
-        centerPadding: '40px',
-        slidesToShow: 1
-      }
-    }
-  ]
-
-});
+	app.mainImageCarousel();
+	app.partners();
 }
 
 $(function() {
