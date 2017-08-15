d7cfdc31cf9eb379e1bc8798e9808caf02b6ffa1
Aken Mehta <mehta.aken@gmail.com>
2017-08-15 14:13:59 -0400

    added main image carousel

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,59 @@
+var app = {};
 
-console.log('connected');
+app.init = function() {
+	console.log('connected app.init');
+
+
+
+$('.main-image-carousel').slick({
+  dots: true,		
+  centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 5,
+
+  responsive: [
+  	{
+      breakpoint: 1500,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 4
+      }
+    },
+    {
+      breakpoint: 1200,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 860,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+
+});
+}
+
+$(function() {
+	app.init();
+});
 
